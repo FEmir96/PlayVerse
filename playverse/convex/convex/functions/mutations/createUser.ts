0c49301345c84fdda2e84a39bd23281286570a9e
Fernando Emir <fernando.faour@davinci.edu.ar>
2025-08-18 23:05:52 -0300

    Correccion del error en createUser.ts
    Se corrigio un error que habia en el codigo y que impedia ejecutar la mutacion desde el servidor Convex para crear o hacer el "insert" de un nuevo usuario

--- playverse/convex/convex/functions/mutations/createUser.ts
+++ playverse/convex/convex/functions/mutations/createUser.ts
@@ -4,15 +4,12 @@ import { v } from "convex/values";
 
 export const createUser = mutation({
   args: {
-    email: v.string(),
-    role: v.union(v.literal("free"), v.literal("premium")),
-  },
-  handler: async ({ db }, { email, role }) => {
-    const now = Date.now();
-    return await db.insert("profiles", {
-      email,
-      role,
-      createdAt: now,
-    });
-  },
+  name: v.string(),
+  email: v.string(),
+  role: v.union(v.literal("free"), v.literal("premium")),
+},
+handler: async ({ db }, { name, email, role }) => {
+  const now = Date.now();
+  return await db.insert("profiles", { name, email, role, createdAt: now });
+},
 });
